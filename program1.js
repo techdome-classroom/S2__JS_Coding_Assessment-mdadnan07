0da23f042447e914efa1789eac8561cbd20e758f
mdadnan07 <131695481+mdadnan07@users.noreply.github.com>
2024-09-25 17:28:03 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,28 @@
-/**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
+    const bracketPairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
     
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        if (bracketPairs[char]) {
+            stack.push(char);
+        } else {
+            let lastOpen = stack.pop();
+            if (bracketPairs[lastOpen] !== char) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
